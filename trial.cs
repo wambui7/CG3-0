0ae6da21b589297262f125a454ee29e1bedfd033
wambui7 <41122098+wambui7@users.noreply.github.com>
2018-07-16 17:04:58 -0500

    Add files via upload
    create a commit

---
+++ trial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class AreaRectangle
+    {
+        static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+            Console.Write("Please write the length of a rectangle:");
+            decimal lengthSide = decimal.Parse(Console.ReadLine());
+            Console.Write("Please write the width of a rectangle:");
+            decimal widthside = decimal.Parse(Console.ReadLine());
+
+            decimal area = lengthSide * widthside;
+
+            
+            Console.WriteLine("The area of the rectangle is: {0}", area);
+               
+
+        }
+    }
+}
