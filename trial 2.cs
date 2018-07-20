bced1e9b066f82c0a054aed600d9768ac9803731
wambui7 <41122098+wambui7@users.noreply.github.com>
2018-07-19 20:56:51 -0500

    Add files via upload

---
+++ trial 2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public Program()
+    {
+        static void Main()
+        {
+            
+            Console.Write("sheila");
+            string stringOne = Console.ReadLine();
+
+            Console.Write("Wambui");
+            string stringtwo = Console.ReadLine();
+
+            Console.Write("Hello");
+            String stringthree = Console.ReadLine();
+
+            string result = "sheila" + "Wambui" + "Hello";
+            Console.Write(result);
+            Console.ReadLine();
+
+
+
+
+
+           
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
